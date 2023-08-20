4c16cead26d36867ad033a113c23e98b82656561
MpGomezG <aome.mggm@gmail.com>
2023-08-20 13:04:09 -0500

    Added new methods in RespuestaDAO, added method "sentimientoAzar".

--- Server/src/co/edu/unbosque/model/RespuestaDAO.java
+++ Server/src/co/edu/unbosque/model/RespuestaDAO.java
@@ -2,9 +2,12 @@ package co.edu.unbosque.model;
 
 import co.edu.unbosque.controller.*;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class RespuestaDAO {
 	private ArrayList<RespuestaDTO> lista;
+	Random r = new Random();
 
 	public RespuestaDAO() {
 		lista = new ArrayList<>();
@@ -18,28 +21,53 @@ public class RespuestaDAO {
 		this.lista = lista;
 	}
 
-	//QUIERO CARGAR EL ARCHIVO DE TEXTO, PERO LA DIRECCION DEL ARCHIVO CAMBIA DEPENDIENDO
+	// QUIERO CARGAR EL ARCHIVO DE TEXTO, PERO LA DIRECCION DEL ARCHIVO CAMBIA
+	// DEPENDIENDO
 	private ArrayList<RespuestaDTO> cargar(String dir) {
-		FileHandler.abrirArchivoTexto(dir);
+		String contenido = FileHandler.abrirArchivoTexto(dir);
+		String[] lineas = contenido.split("\n");
+		for (String linea : lineas) {
+			String[] attrs = linea.split(".");
+			if (attrs.length == 2) {
+				String num = attrs[0];
+				String res = attrs[1];
+				lista.add(new RespuestaDTO(num, res));
+			}
+		}
 		return lista;
 	}
 	
+	private String sentimientoAzar() {
+		List<String> sentimientos = new ArrayList<>();
+		String contenido = FileHandler.abrirArchivoTexto("sentimiento.txt");
+		String[] lineas = contenido.split("\n");
+		for(String linea : lineas) {
+			sentimientos.add(linea);
+		}
+		int randomNum = r.nextInt(sentimientos.size());
+		String sentimientoRandom = sentimientos.get(randomNum);
+		return "" + sentimientoRandom;
+	}
+
 	private void pedirNum(int num) {
+		cargar("chatbot.txt");
 		switch (num) {
 		case 1: {
-			
-		return;
+			sentimientoAzar();
+
+			return;
 		}
 		case 2:
-			
+			cargar(null);
+
 			return;
-		case 3: 
-			
+		case 3:
+
 			return;
-		
+
 		default:
 			throw new IllegalArgumentException("Unexpected value: " + num);
 		}
-		
+
 	}
 }
