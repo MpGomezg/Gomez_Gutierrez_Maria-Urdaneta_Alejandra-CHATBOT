e453a449ee56c8f555ffdd0a638adce4dff462d0
MpGomezG <aome.mggm@gmail.com>
2023-08-20 11:21:17 -0500

    Added class RespuestaDAO.

--- Server/src/co/edu/unbosque/model/RespuestaDAO.java
+++ Server/src/co/edu/unbosque/model/RespuestaDAO.java
@@ -1,10 +1,11 @@
 package co.edu.unbosque.model;
 
+import co.edu.unbosque.controller.*;
 import java.util.ArrayList;
 
 public class RespuestaDAO {
 	private ArrayList<RespuestaDTO> lista;
-	
+
 	public RespuestaDAO() {
 		lista = new ArrayList<>();
 	}
@@ -17,4 +18,28 @@ public class RespuestaDAO {
 		this.lista = lista;
 	}
 
+	//QUIERO CARGAR EL ARCHIVO DE TEXTO, PERO LA DIRECCION DEL ARCHIVO CAMBIA DEPENDIENDO
+	private ArrayList<RespuestaDTO> cargar(String dir) {
+		FileHandler.abrirArchivoTexto(dir);
+		return lista;
+	}
+	
+	private void pedirNum(int num) {
+		switch (num) {
+		case 1: {
+			
+		return;
+		}
+		case 2:
+			
+			return;
+		case 3: 
+			
+			return;
+		
+		default:
+			throw new IllegalArgumentException("Unexpected value: " + num);
+		}
+		
+	}
 }
